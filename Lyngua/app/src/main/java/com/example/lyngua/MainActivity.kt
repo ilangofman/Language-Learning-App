7bec2fc9e16c989bff10a655d14a55ea2620aeef
Daniel Truong <danieltruong@cmail.carleton.ca>
2020-09-28 18:57:43 -0400

    Basic framework for bottom navigation bar

--- Lyngua/app/src/main/java/com/example/lyngua/MainActivity.kt
+++ Lyngua/app/src/main/java/com/example/lyngua/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.lyngua
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
