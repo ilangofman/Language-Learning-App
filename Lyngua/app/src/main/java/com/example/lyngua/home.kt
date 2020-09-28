df31b73daa86cf768808c3757191d5ec7a05d75a
Daniel Truong <danieltruong@cmail.carleton.ca>
2020-09-28 18:57:43 -0400

    Basic framework for bottom navigation bar

---
+++ Lyngua/app/src/main/java/com/example/lyngua/home.kt
@@ -0,0 +1,25 @@
+package com.example.lyngua
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [home.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class home : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_page, container, false)
+    }
+
+}
