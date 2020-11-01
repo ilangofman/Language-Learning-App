fb61b9aebce1911712eeaaeef412bd8449c6fb38
Daniel Truong <danieltruong@cmail.carleton.ca>
2020-11-01 11:34:41 -0500

    Removed comments from session class

--- Lyngua/app/src/main/java/com/example/lyngua/controllers/Session.kt
+++ Lyngua/app/src/main/java/com/example/lyngua/controllers/Session.kt
@@ -5,31 +5,6 @@ import com.example.lyngua.models.words.Word
 
 class Session(val category: Category) {
 
-    //Goes through the list of words using the wordIdList and generates the question
-    //Inputs are
-//    fun runSession(wordIdList: ArrayList<Int>){
-
-//        var currentId = 0
-//        println("Session Number: ${category.sessionNumber}")
-//        for (word in category.wordsList){
-//            if(wordIdList.size > currentId) {
-//
-//                //Only run if there are still words in the session
-//                if (word.id == wordIdList[currentId]) {
-//                     when (word.streak){
-//                         //in  1 .. 4 -> multipleChoice(word)
-//                         in  5 .. 7 -> wordMatching(word)
-//                         in 8 .. 10 -> fillInTheBlank(word)
-//                    }
-//
-//                    currentId++
-//                }
-//            }
-//        }
-//        category.sessionNumber++
-//        val questions = generateSession()
-//    }
-
     //Chooses the words that should be part of the session based on the sessionNumber and boxNumbers
     //Input parameter is the category that is being played
     //Returns an array of integers for the id of words to be used in the session
@@ -152,55 +127,4 @@ class Session(val category: Category) {
 
         return questionsList
     }
-
-//Function to be integrated with multiple choice view
-//TODO change so that the correct option for the word in MC questions is done during runtime of the session
-//NOTE: This function will need to be modified
-//    private fun multipleChoice(word: Word){
-//        //Switching between type translation vs english
-//        if (word.typeFlag % 2 == 0) {
-//
-//            //THIS IS TO PRINT WORDS, IS NOT NEEDED
-//            //Randomize the order of the options
-//            word.option = (1..4).random()
-//            println("----${word.word}----")
-//            var j = 0
-//            for (i in 1..4) {
-//                if (i == word.option) {
-//                    println("${word.option}: ${word.translated}")
-//                } else {
-//                    //Printing of these options for words will depend on if it is from translated to english or vice versa
-//                    println("$i: ${word.wordsList[j]}")
-//                    j++
-//                }
-//            }
-//
-//            //User input test
-//            print("\nWhat is the correct translation?: ")
-//            var answer = readLine()?.toInt()
-//            word.updateWord(answer)
-//
-//        }
-//        else {
-//            //THIS IS TO PRINT WORDS, IS NOT NEEDED
-//            word.option = (1..4).random()
-//            println("----${word.translated}----")
-//            var j = 0
-//
-//            for (i in 1..4) {
-//                if (i == word.option) {
-//                    println("${word.option}: ${word.word}")
-//                } else {
-//                    //Printing of these options for words will depend on if it is from translated to english or vice versa
-//                    println("$i: ${word.wordsList[j]}")
-//                    j++
-//                }
-//            }
-//
-//            //User input test
-//            print("\nWhat is the correct translation?: ")
-//            var answer = readLine()?.toInt()
-//            word.updateWord(answer)
-//        }
-//    }
 }
