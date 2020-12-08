c5b87edc7bf52240cab148f759a4f9c6e3c2cf1b
Daniel Truong <danieltruong@cmail.carleton.ca>
2020-12-08 12:05:26 -0500

    Deliverable 4 Custom Change

--- Lyngua/app/src/main/java/com/example/lyngua/views/Categories/goals/TimeIntervalGoal.kt
+++ Lyngua/app/src/main/java/com/example/lyngua/views/Categories/goals/TimeIntervalGoal.kt
@@ -148,7 +148,8 @@ class TimeIntervalGoal(arg: UpdateCategoryArgs) : Fragment() {
                 0 -> args.categoryChosen.goal.cancelAlarms(requireContext(), args.categoryChosen)
                 1 -> myCalendar.add(Calendar.DAY_OF_MONTH, 1)
                 2 -> myCalendar.add(Calendar.DAY_OF_MONTH, 7)
-                3 -> myCalendar.add(Calendar.MONTH, 1)
+                //3 -> myCalendar.add(Calendar.MONTH, 1)
+                3 -> myCalendar.add(Calendar.MINUTE, 2)
             }
 
             //Creates a goal object based on the options chosen from updating to be put into the database
